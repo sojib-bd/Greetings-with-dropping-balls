f58fb119b63436176c58855763a9d12aee106225
sojib-bd <sojibrahmatuzzaman@gmail.com>
2018-10-17 04:45:57 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+         var x=document.getElementById("start")
+         x.addEventListener("click",function(){
+			red.style.animation= "twinkle 2s infinite";
+		});
+		start.addEventListener("click",function(){
+			green.style.animation= "twinkle 2s infinite";
+		});
+		/*
+		start.addEventListener("click",function(){
+			blue.style.animation= "twinkle 2s infinite";
+		});
+		start.addEventListener("click",function(){
+			yellow.style.animation= "twinkle 2s infinite";
+		})
+		start.addEventListener("click",function(){
+			ball-5.style.animation= "twinkle 2s infinite";
+		})
+		start.addEventListener("click",function(){
+			ball-6.style.animation= "twinkle 2s infinite";
+		})
+		start.addEventListener("click",function(){
+			ball-7.style.animation= "twinkle 2s infinite";
+		})
+		start.addEventListener("click",function(){
+			ball-8.style.animation= "twinkle 2s infinite";
+		})
+		start.addEventListener("click",function(){
+			ball-9.style.animation= "twinkle 2s infinite";
+		})
+		start.addEventListener("click",function(){
+			ball-10 .style.animation= "twinkle 2s infinite";
+		})*/
